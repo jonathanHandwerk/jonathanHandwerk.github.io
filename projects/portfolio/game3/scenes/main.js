ec10b4f5c04447e50434eab3dbdd14446ac74137
thats a secret <jlhofstra@yahoo.com>
2021-06-07 21:23:51 +0900

    added some ground

--- projects/portfolio/game3/scenes/main.js
+++ projects/portfolio/game3/scenes/main.js
@@ -17,7 +17,7 @@ const levelCfg = {
   height: 20,
   'x': [sprite('ground'), solid()],
   '*': [sprite('Panario')],
-  '?': [sprite('question'), 'question-mark', solid(), scale(0.8)],
+  '?': [sprite('question'), 'gyaku', solid(), scale(0.8)],
   '!': [sprite('bikuri'), 'hamburg-surprise', solid(), scale(0.8)],
   '%': [sprite('hamburg'), 'hamburg', body()],
   '+': [sprite('gyaku'), 'gyaku', body()],
@@ -90,8 +90,11 @@ keyPress('space', ()=> {
 player.on('headbump', (obj) => {
   if(obj.is('hamburg-surprise')) {
     gameLevel.spawn('%', obj.gridPos.sub(0,1))
-    
-  }})
+}
+if(obj.is('gyaku')) {
+    gameLevel.spawn('+', obj.gridPos.sub(0,1))
+}})
+
 
 action('hamburg', (h)=> {
     h.move(20, 0)
@@ -99,5 +102,9 @@ action('hamburg', (h)=> {
 player.collides('hamburg', (h) => {
   player.biggify(6)
   destroy(h)
+})
+player.collides('gyaku', (g) => {
+  scoreLabel.value++
+  scoreLabel.text = scoreLabel.value
 })
 
